481bd2810193ca49bb7006cc094bf62ff9f1219b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-19 20:31:35 +0100

    ability unlock

---
+++ AbilityUnlock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityUnlock : MonoBehaviour
+{
+    public bool canDoubleJump;
+    public bool canDash;
+    public bool canBall;
+    public bool canBomb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
