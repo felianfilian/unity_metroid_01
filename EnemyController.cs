992c424539b7ee63eb8ff5bfb889650e764841a4
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-24 18:39:39 +0100

    create enemy controller

---
+++ EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public Transform[] patrolPoints;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
