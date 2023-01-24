ad4233147ad65b3f28c63491b734570dbfe03c16
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-24 20:28:51 +0100

    enemy moving

--- EnemyController.cs
+++ EnemyController.cs
@@ -7,15 +7,31 @@ public class EnemyController : MonoBehaviour
     public Rigidbody2D rigidbody;
     public Transform[] patrolPoints;
 
+    public float moveSpeed = 5f;
+    public float waitAtPoint = 3f;
+    public float jumpForce = 10f;
+    
+    private int currentPoint;
+    private float waitcounter;
 
     void Start()
     {
-        
+        waitcounter = waitAtPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(transform.position.x - patrolPoints[currentPoint].transform.position.x) > 0.2f)
+        {
+            if(transform.position.x < patrolPoints[currentPoint].transform.position.x)
+            {
+                rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
+            }
+            else
+            {
+                rigidbody.velocity = new Vector2(-moveSpeed, rigidbody.velocity.y);
+            }
+        }
     }
 }
