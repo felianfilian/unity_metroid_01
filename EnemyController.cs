e959240ee799bc13b21e2f17b0f72e75f08146a5
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-02-25 21:34:00 +0100

    enemy jumping

--- EnemyController.cs
+++ EnemyController.cs
@@ -10,12 +10,14 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed = 5f;
     public float waitAtPoint = 3f;
     public float jumpForce = 10f;
-    
+
+    private int direction;
     private int currentPoint;
     private float waitcounter;
 
     void Start()
     {
+        direction = 1;
         currentPoint = 0;
         waitcounter = waitAtPoint;
     }
@@ -35,6 +37,10 @@ public class EnemyController : MonoBehaviour
                 rigidbody.velocity = new Vector2(-moveSpeed, rigidbody.velocity.y);
                 transform.localScale = new Vector3(1f, 1f, 1f);
             }
+            if(transform.position.y < patrolPoints[currentPoint].transform.position.y - 0.3f && rigidbody.velocity.y <0.1f)
+            {
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+            }
         }
         else
         {
@@ -43,10 +49,15 @@ public class EnemyController : MonoBehaviour
             if(waitcounter <= 0)
             {
                 waitcounter = waitAtPoint;
-                currentPoint++;
-                if(currentPoint >= patrolPoints.Length)
+                currentPoint += direction;
+                Debug.Log(direction);
+                if (currentPoint >= patrolPoints.Length-1)
                 {
-                    currentPoint = 0;
+                    direction = -1;
+                }
+                if(currentPoint == 0)
+                {
+                    direction = 1;
                 }
             }
         }
