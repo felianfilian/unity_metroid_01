474c8fd250e0df3468e0a5666f333ef987f6ed0b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-03-04 00:59:21 +0100

    fly enemy setup

--- EnemyFlyController.cs
+++ EnemyFlyController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class EnemyFlyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
+
+    public float chaseRange = 10f;
+    public float moveSpeed = 5f;
+    public float turnSpeed = 5f;
+    
+    private bool isChasing;
+    private Transform player;
+
     void Start()
     {
-        
+        player = PlayerHealthController.instance.transform;
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        FlyerMove();
+    }
+
+    public void FlyerMove()
+    {
+        if (!isChasing)
+        {
+            if (Vector3.Distance(transform.position, player.position) < chaseRange)
+            {
+                isChasing = true;
+            }
+        }
+        else
+        {
+            if (player.gameObject.activeSelf)
+            {
+                Vector3 direction = transform.position - player.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Quaternion targetRot = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, turnSpeed * Time.deltaTime);
+                transform.position += transform.right * moveSpeed * Time.deltaTime;
+                animator.SetBool("isChasing", isChasing);
+            }
+        }
     }
 }
